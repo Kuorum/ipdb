12cf937fb59329c9d25863e6c86a6a0b66ae0928
Ryzal Yusoff <ryzalyusoff@gmail.com>
2015-11-02 00:47:08 +0000

    Fix political party image

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,6 +2,10 @@ class Region < ActiveRecord::Base
 	has_many :political_parties
 
 	def iso_and_name    
+    	"#{self.iso3166_2} --- " " #{self.name.titleize }"   
+  	end
+
+  	def iso_and_name2   
     	"#{self.iso3166_2} --- " " #{self.name.titleize } (#{self.id})"   
   	end
 
